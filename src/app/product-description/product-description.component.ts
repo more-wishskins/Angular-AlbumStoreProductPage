9b600df1d143396ba2e4cb2076854e335f194a52
Whit <may.whitney21@gmail.com>
2021-03-26 10:34:57 -0600

    mod 2.1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-product-page',
-  templateUrl: './product-page.component.html',
-  styleUrls: ['./product-page.component.css']
-})
-export class ProductPageComponent implements OnInit {
+import { ProductService } from '../product.service';
 
-  constructor() { }
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+  albumInfo;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
